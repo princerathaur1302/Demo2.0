0817bc676ff724dec0532d4a79e84410cdbdeae6
princerathaur1302 <princerathaur1302@gmail.com>
2025-06-27 08:25:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -655,12 +655,12 @@ async def txt_handler(bot: Client, m: Message):
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
 
             try:
-                cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** `{name1} [{res}p] .mkv`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}` https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
-                cc1 = f'[📕]Pdf Id : {str(count).zfill(3)}\n**File Title :** `{name1} .pdf`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}` https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
-                cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** `{name1} .zip`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}` https://t.me/addlist/C4FsGVXQeGRkZjM1 \n' 
-                ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** `{name1} .jpg`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}` https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
-                ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** `{name1} .mp3`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}` https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
-                cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** `{name1} .html`\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**`{CR}` https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
+                cc = f'[🎥]Vid Id : {str(count).zfill(3)}\n**Video Title :** {name1} [{res}p].mkv\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**{CR} https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
+                cc1 = f'[📕]Pdf Id : {str(count).zfill(3)}\n**File Title :** {name1}.pdf\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**{CR} https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
+                cczip = f'[📁]Zip Id : {str(count).zfill(3)}\n**Zip Title :** {name1}.zip\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**{CR} https://t.me/addlist/C4FsGVXQeGRkZjM1 \n' 
+                ccimg = f'[🖼️]Img Id : {str(count).zfill(3)}\n**Img Title :** {name1}.jpg\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**{CR} https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
+                ccm = f'[🎵]Audio Id : {str(count).zfill(3)}\n**Audio Title :** {name1}.mp3\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**{CR} https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
+                cchtml = f'[🌐]Html Id : {str(count).zfill(3)}\n**Html Title :** {name1}.html\n<pre><code>**Batch Name :** {b_name}</code></pre>\n\n**Extracted by➤**{CR} https://t.me/addlist/C4FsGVXQeGRkZjM1 \n'
                   
                 if "drive" in url:
                     try:
